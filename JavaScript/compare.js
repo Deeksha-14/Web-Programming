4bcca0ed5f834a2da131552300fd8ebf79bafe38
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-05-30 22:00:12 +0530

    Create compare.js

---
+++ JavaScript/compare.js
@@ -0,0 +1,16 @@
+ function compare(n1)
+            {
+                var a=parseInt(document.getElementById('n1').value);
+               
+              alert('n1');
+                if(a<200)
+                {
+                    alert("number is smaller than 200");
+                }
+                else if(a>200)
+                {
+                    document.write("number is greater than 200");
+                }
+                else  
+                alert("number is 200")
+            }
